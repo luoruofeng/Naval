eaf5db3534405219a830a4b5c6e6e2e4204c4281
luoruofeng <717750878@qq.com>
2023-06-06 16:50:05 +0800

    删除registry的相关代码，不打算使用项目启动registry，把耦合度降低

--- fx_opt/var.go
+++ fx_opt/var.go
@@ -4,26 +4,16 @@ import (
 	"context"
 
 	"github.com/luoruofeng/Naval/srv"
-	"go.uber.org/zap"
 )
 
 // 添加其他需要在fx中构建的实例的方法
 var ConstructorFuncs = []interface{}{
 	srv.NewTask,
-	srv.NewRegistry,
 }
 
 // 在ConstructorFuncs添加了方法后，如果需要在方法的参数中传递fx.Lifecycle，已实现fx.Hook。需要在下方添加fx的invoke方法。
 var InvokeFuncs = []interface{}{
 	func(ts srv.TaskSrv, cancel context.CancelFunc) {
 		go ts.InitWorkerpools()
-	},
-	func(rs srv.RegistryServer, logger *zap.Logger) {
-		logger.Info("registry服务开启端口监听")
-		err := rs.Registry.ListenAndServe()
-		if err != nil {
-			logger.Error("registry listen and serve error", zap.Error(err))
-			panic(err)
-		}
 	},
 }
